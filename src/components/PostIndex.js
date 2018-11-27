5899fdac834967499f5b303a6583c08b9d8beafe
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-27 14:24:44 +0100

    some cleanup

---
+++ src/components/PostIndex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostIndex extends React.Component {
+    render(){
+        return (
+            <div>
+                PostIndex
+            </div>
+        );
+    };
+};
+
+export default PostIndex;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/App';
-
-class Hello extends React.Component {
-    render(){
-        return <div>Hello</div>
-    }
-}
-
-class Goodbye extends React.Component {
-    render(){
-        return <div>Goodbye</div>
-    }
-}
+import PostIndex from './components/PostIndex';
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
-            <Route path="/hello" component={Hello} />
-            <Route path="/goodbye" component={Goodbye} />
+            <Route path="/" component={PostIndex} />
         </div>
     </BrowserRouter>,
     document.querySelector('#root')
