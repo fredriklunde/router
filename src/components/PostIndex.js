257dd36eee08230e16b59855bbb3ad219e1c48f5
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-27 15:19:25 +0100

    redux form

--- src/components/PostIndex.js
+++ src/components/PostIndex.js
@@ -20,7 +20,6 @@ class PostIndex extends React.Component {
     }
 
     render(){
-        console.log(this.props.posts)
         return (
             <div>
                 <div className="text-xs-right">
