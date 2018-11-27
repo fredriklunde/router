c92ea6a7aa4d5756bd35ce2fe5d6f9cafcf1795a
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-27 15:38:31 +0100

    nice

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -2,21 +2,39 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
-    renderTitleField(field){
+    renderField(field){
         return(
-            <div>
+            <div className="form-group">
+                <label>{field.label}</label>
                 <input
+                    className="form-control"
+                    type={field.text}
                     {...field.input}
                 />
             </div>
         )
     }
+
     render(){
         return (
             <form>
                 <Field
+                    label="Title"
                     name="title"
-                    component={this.renderTitleField}
+                    type="text"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    type="text"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    type="text"
+                    component={this.renderField}
                 />
             </form>
         );
