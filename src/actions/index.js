1c4aa0b86988384636e080eaf1722da0c58d252b
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-29 20:32:14 +0100

    get data for specific post

--- src/actions/index.js
+++ src/actions/index.js
@@ -29,7 +29,6 @@ export function createPost(values, callback){
 
 export function fetchPost(id){
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
-
   return {
     type: FETCH_POST,
     payload: request
--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -4,21 +4,28 @@ import { fetchPost } from '../actions';
 
 class PostShow extends React.Component {
     componentDidMount() {
-        const { id } = this.props.match.params.id;
+        const id  = this.props.match.params.id;
         this.props.fetchPost(id);
     }
 
     render() {
+        const { post } = this.props;
+
+        if(!post){
+            return <div> Loading</div>
+        }
         return (
             <div>
-                PostShow
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }   
 }
 
-function mapStateToProps({ posts }) {
-
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id]}
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostShow);
