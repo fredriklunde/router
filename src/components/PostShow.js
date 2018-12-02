ef2ec60f4da67303d4af9081dbca138b97d76d50
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-02 09:02:49 +0100

    some links

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostShow extends React.Component {
@@ -12,10 +13,12 @@ class PostShow extends React.Component {
         const { post } = this.props;
 
         if(!post){
-            return <div> Loading</div>
+            return <div>Loading</div>
         }
+        
         return (
             <div>
+                <Link to="/">Back To Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
