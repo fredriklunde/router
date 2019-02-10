b2af0bcad26f93b1934f5ddb99b175a2d1c797cb
fredriklunde <fredrik.lunde3@gmail.com>
2019-02-10 09:41:33 +0100

    Updates

---
+++ precache-manifest.a2a4287e7dbf8f979c9ac25eb201a8ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbaaad6f7306bb5f5a6",
+    "url": "/router/static/css/main.7c56c8f4.chunk.css"
+  },
+  {
+    "revision": "efbaaad6f7306bb5f5a6",
+    "url": "/router/static/js/main.efbaaad6.chunk.js"
+  },
+  {
+    "revision": "64080119dbb153bbf759",
+    "url": "/router/static/js/1.64080119.chunk.js"
+  },
+  {
+    "revision": "e284ae029e69c996dae8",
+    "url": "/router/static/js/runtime~main.e284ae02.js"
+  },
+  {
+    "revision": "b2484dfca9c012b8e918f1f5f5ca2c33",
+    "url": "/router/index.html"
+  }
+];
